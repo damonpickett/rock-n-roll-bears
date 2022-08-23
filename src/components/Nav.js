6786f611694157fe228d6e525f13d3706aa7b07f
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-08-23 15:43:54 -0700

    added burger button and nav buttons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,17 @@
+import * as FaIcons from 'react-icons/fa';
+
 function Nav() {
     return (
       <div className="nav">
-        Nav
+        <div className='nav-container'>
+            <div className='burger'>
+            <FaIcons.FaBars />
+            </div>
+            <div className='nav-buttons'>
+                <button className='nav-button'>Instructions</button>
+                <button className='nav-button'>Connect Wallet</button>
+            </div>
+        </div>
       </div>
     );
   }
