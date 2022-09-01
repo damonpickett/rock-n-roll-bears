0603f9a6cf2d1e8b6860b0da67993bb89a4aa10e
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-09-01 14:42:44 -0700

    added about content and styled

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,8 @@
 function About() {
     return (
       <div className="about">
-        About
+        <h2>The Story so far...</h2>
+        <p>LVX TOTAL was a secret government project to develop a serum that would allow humans to domesticate wild animals. But when the serum was tested on four wild bears, it not only failed, but gave the bears human qualities (most notably a lust for life!). The four bears escaped the government research site and headed straight to rock ‘n’ roll stardom. Now the Rock ‘n’ Roll Bears tour the world, blowing fans away with their transcendent live performances while thwarting attempts at censorship by Quintin Pendergrast, head of the Concerned Parents of America Organization (even though he doesn’t even have kids!) Will the Bears rock on? Or will the forces of buzz-killing silence them once and for all? Get your Rock ‘n’ Roll Bear NFT today, join the community, and help bring this story to life!</p>
         <div id='roadmap'></div>
       </div>
     );
