4cd1e93e68765e0ec09e755c76f0aee4254db7a7
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-09-09 20:23:22 -0700

    added background color, dummy text, padding and margin to roadmap cards

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -2,9 +2,30 @@ function Roadmap() {
     return (
       <div className="roadmap">
         <h2>Roadmap</h2>
-        <p>Phase 1</p>
         <div className='roadmap-card'>
-
+          
+          <h3>Phase 1</h3>
+          <ul className='phase-list'>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit.</li>
+          </ul>
+        </div>
+        <div className='roadmap-card'>
+          <h3>Phase 2</h3>
+          <ul className='phase-list'>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit.</li>
+          </ul>
+        </div>
+        <div className='roadmap-card'>
+          <h3>Phase 3</h3>
+          <ul className='phase-list'>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit.</li>
+          </ul>
         </div>
         <div id='social'></div>
       </div>
